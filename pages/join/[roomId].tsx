567cd720d6532de404482b69009e3ab27a9cb7a2
Nathan Cheung <nathancheung22@gmail.com>
2022-12-27 22:05:15 -0500

    Create [roomId].tsx

---
+++ pages/join/[roomId].tsx
@@ -0,0 +1,14 @@
+import { useRouter } from "next/router";
+import React from "react";
+
+export const Join = () => {
+  const router = useRouter();
+  const { roomId } = router.query;
+
+  return (
+    <>
+      <h1>yeetcode</h1>
+      <p>joining room with id {roomId}</p>
+    </>
+  );
+};
